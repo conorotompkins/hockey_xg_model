4d287b3cf1e089250e085e8dffc302e2a72d90e2
Conor <conorotompkins@gmail.com>
2019-05-25 15:41:39 -0400

    cleaned up viz code

--- scripts/visualization.R
+++ scripts/visualization.R
@@ -4,8 +4,14 @@ library(gganimate)
 
 theme_set(theme_bw())
 
-df <- read_csv("xg_model/data/Shot Prediction Data for Comparison.csv") %>% 
+df <- read_csv("data/Shot Prediction Data for Comparison.csv") %>% 
   clean_names()
+
+df %>% 
+  ggplot(aes(coords_x, coords_y)) +
+  #geom_point()
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  scale_fill_viridis_c()
 
 df %>% 
   #filter(str_detect(event_player_1, "CROSBY") | str_detect(event_player_1, "OVECHKIN")) %>% 
@@ -26,35 +32,26 @@ df %>%
 
 df %>% 
   ggplot(aes(coords_x, coords_y)) +
-  #geom_point()
-  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
-  scale_fill_viridis_c()
-
-df %>% 
-  ggplot(aes(coords_x, coords_y)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c() +
   facet_wrap(~shot_type) 
 
 df %>% 
   ggplot(aes(coords_x, coords_y)) +
-  geom_point() +
-  #stat_density_2d(aes(alpha = stat(level), fill = shot_type), geom = "polygon") +
+  #geom_point() +
+  stat_density_2d(aes(alpha = stat(level), fill = shot_type), geom = "polygon") +
   scale_fill_viridis_d() +
   transition_states(shot_type) +
   enter_fade() + 
   exit_shrink() +
   ease_aes("linear")
 
-df %>% 
-  ggplot(aes(coords_x)) +
-  geom_histogram()
-
 df_shooting_tile <- df %>% 
   select(coords_x, coords_y, goal) %>% 
   group_by(coords_x, coords_y) %>% 
   summarize(shoot_percent = mean(goal == TRUE),
             n = n()) %>% 
+  ungroup() %>% 
   complete(coords_x = c(-98:98), coords_y = c(-50:50)) %>% 
   replace_na(list(shoot_percent = 0, n = 0))
 
@@ -64,10 +61,11 @@ df_shooting_tile %>%
 
 df_shooting_tile %>% 
   ggplot(aes(n, shoot_percent)) +
-  geom_point()
+  geom_jitter(alpha = .1) +
+  geom_smooth()
 
 df_shooting_tile %>% 
-  ggplot(aes(coords_x, coords_y, fill = shoot_percent, alpha = n)) +
+  ggplot(aes(coords_x, coords_y, fill = shoot_percent)) +
   geom_tile() +
   #stat_density_2d(aes(fill = shoot_percent), geom = "polygon") +
   scale_fill_viridis_c() +
